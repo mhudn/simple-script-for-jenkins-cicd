e57d7af870cba159a2ed8363fa3ca767d9afc64a
mhudn <mohi@mec.science>
2023-08-02 19:57:47 -0400

    simple script

---
+++ simple-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+echo "This is an Samples Script from GitHub"
+echo "This is test from Linux Server"
+echo "This is running in a build step and it shows changes"
+SUM=$((FIRSTNUMBER + SECONDNUMBER))
+echo "$SUM"
